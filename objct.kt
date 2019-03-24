64e77a696cb97012f3f1a94f0e9e1b2577d0d57f
kab <kab3nan9@gmail.com>
2019-03-25 02:50:37 +0900

    プロパティ

--- objct.kt
+++ objct.kt
@@ -10,6 +10,17 @@ interface Bucket{
     fun setQuantity(quantity:Int)
 }
 
+//プロパティ
+interface Bucket2{
+    fun fill()
+    fun drainAway()
+    fun pourTo(that: Bucket2)
+
+    //変数ではなくプロパティ
+    val capacity:Int
+    var quantity:Int
+}
+
 fun createBucket(capacity:Int):Bucket = object:Bucket{
     var _quantity:Int = 0
 
@@ -27,7 +38,8 @@ fun createBucket(capacity:Int):Bucket = object:Bucket{
         if(getCapacity() <= thatVacuity){
             that.setQuantity(that.getQuantity() + getQuantity())
             drainAway()
-        }else{
+        }
+        else{
             that.fill()
             setQuantity(getQuantity() - thatVacuity)
         }
@@ -39,6 +51,33 @@ fun createBucket(capacity:Int):Bucket = object:Bucket{
 
     override fun setQuantity(quantity:Int){
         _quantity = quantity
+    }
+}
+
+fun createBucket2(_capacity:Int):Bucket2 = object:Bucket2{
+
+    override val capacity = _capacity
+
+    override var quantity:Int = 0
+
+    override fun fill(){
+        quantity = capacity
+    }
+
+    override fun drainAway(){
+        quantity = 0
+    }
+
+    override fun pourTo(that:Bucket2){
+        val thatVacuity = that.capacity - that.quantity
+        if(capacity <= thatVacuity){
+            that.quantity += quantity
+            drainAway()
+        }
+        else{
+            that.fill()
+            quantity -= thatVacuity
+        }
     }
 }
 
@@ -73,7 +112,6 @@ fun main(args:Array<String>){
     bucket.printQuantity()
 
     //インタフェース
-
     //容量12のバケツ
     val bucket2 = createBucket(12)
     //容量7のバケツ
@@ -87,4 +125,20 @@ fun main(args:Array<String>){
 
     println(bucket2.getQuantity())
     println(bucket3.getQuantity())
+
+    //プロパティ
+    //容量8のバケツ
+    val bucket4 = createBucket2(8)
+    //容量3のバケツ
+    val bucket5 = createBucket2(3)
+
+    //バケツ4を満たす
+    bucket4.fill()
+
+    //バケツ4からバケツ5へ可能な限りそそぐ
+    bucket4.pourTo(bucket5)
+
+    println(bucket4.quantity)
+    println(bucket5.quantity)
+
 }

1804892a32a6875738fb1b2b0ec8ee87aa216e55
kab <kab3nan9@gmail.com>
2019-03-30 02:57:09 +0900

    継承について

---
+++ class_use.kt
@@ -0,0 +1,33 @@
+open class Person(val name:String){ //kotlinでは継承させるために修飾子openが必要
+    open fun introduceMyself(){
+        println("I am $name")
+    }
+}
+
+//継承
+class Student(name:String, val id:Long):Person(name)
+//オーバーライド(メンバを上書き)
+class Student2(name:String, val id:Long):Person(name){
+    override fun introduceMyself(){
+        println("I am $name(id=$id)")
+
+        //オーバーライド前のメンバを使うとき
+        println("id無しver")
+        super.introduceMyself()
+    }
+}
+//openやoverrideを付ければプロパティやセッター・ゲッターも再定義可能
+
+fun main(args:Array<String>){
+    //継承
+    val person:Person = Person("みく")
+    person.introduceMyself()
+
+    val student:Student = Student("百合子",417)
+    println(student.id)
+    println(student.name)
+    student.introduceMyself() //スーパクラスのメンバをサブクラスも自動で持つようになる
+
+    val student2:Student2 = Student2("七尾",712)
+    student2.introduceMyself()
+}

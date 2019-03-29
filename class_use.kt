9b2f4572966fdf9a7bed4bd9a821fc6a9acf062b
kab <kab3nan9@gmail.com>
2019-03-30 03:14:56 +0900

    抽象クラスについて

--- class_use.kt
+++ class_use.kt
@@ -18,6 +18,25 @@ class Student2(name:String, val id:Long):Person(name){
 }
 //openやoverrideを付ければプロパティやセッター・ゲッターも再定義可能
 
+//抽象hoge(hogeのオーバーライドを強制する)
+//抽象クラス
+abstract class Greeter(val target:String){
+    abstract fun sayHello() //抽象メンバ
+}
+
+class EnglishGreeter(target:String):Greeter(target){
+    override fun sayHello(){
+        println("Hello $target!")
+    }
+}
+
+class JapaneseGreeter(target:String):Greeter(target){
+    override fun sayHello(){
+        println("こんにちは、$target!")
+    }
+}
+
+//継承の記述を省略した場合自動的にクラスAnyを継承することになる
 fun main(args:Array<String>){
     //継承
     val person:Person = Person("みく")
@@ -35,4 +54,9 @@ fun main(args:Array<String>){
     val person2:Person = Student2("伊藤",39) //実態はStudent2インスタンスだが見かけ上の方はPerson
     person2.introduceMyself()
     //person.id エラーが出るためコメントアウト
+    //見かけ上の型はPersonであるためStudent2で追加したプロパティidにはアクセスできない
+
+    //抽象メンバ
+    EnglishGreeter("Kotlin").sayHello()
+    JapaneseGreeter("java").sayHello()
 }

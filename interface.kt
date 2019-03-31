cf040010dfba15cf977d0a598f6a1da314c90eb3
kab <kab3nan9@gmail.com>
2019-03-31 17:31:38 +0900

    コンフリクトの回避

--- interface.kt
+++ interface.kt
@@ -13,7 +13,72 @@ class EnglishGreeter:Greeter{
     }
 }
 
+//インタフェース同士で同一シグネチャ
+interface Foo{
+    fun execute()
+}
+
+interface Bar{
+    fun execute()
+}
+
+class FooBar:Foo, Bar{
+    override fun execute(){
+        println("FooBar")
+    }
+}
+//オーバーライドすれば問題ない
+
+//クラスとインタフェースで同一シグネチャ
+interface Fee{
+    fun execute()
+}
+
+open class Superclass{
+    open fun execute(){
+        println("superclass")
+    }
+}
+
+class FeeSubclass:Superclass(), Fee{
+    override fun execute(){
+        println("FeeSubclass")
+    }
+}
+//Superclassで既に実装されているためクラスFeeSubclassにオーバーライド義務はない
+
+//実装を持った二つのインタフェース
+interface Hoge{
+    fun execute(){
+        println("hoge")
+    }
+}
+
+interface Fuga{
+    fun execute(){
+        println("fuga")
+    }
+}
+
+class HogeFuga:Hoge,Fuga{
+    //このままではどちらのexecuteを使うかあいまいなためコンパイルに失敗する
+    //この場合既に提供されている実装を使いたくてもオーバーライドする必要がある
+    override fun execute(){
+        super<Hoge>.execute() //これでオーバーライドしたうえでインタフェースHogeの実装を利用できる
+    }
+}
+
 fun main(args:Array<String>){
     val english:EnglishGreeter = EnglishGreeter()
     english.sayHello(english.language)
+
+    val foobar:FooBar = FooBar()
+    foobar.execute()
+
+    val feesub:FeeSubclass = FeeSubclass()
+    feesub.execute()
+    Superclass().execute()
+
+    val hogefuga:HogeFuga = HogeFuga()
+    hogefuga.execute()
 }

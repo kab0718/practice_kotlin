e01bfa42b2ec2c55a273aaea706486bcaa4a7eab
kab <kab3nan9@gmail.com>
2019-04-02 01:19:01 +0900

    ジェネリクス関数

--- generics.kt
+++ generics.kt
@@ -1,11 +1,23 @@
 //ジェネリクスの導入
 class Container<T>(var value:T) //<T>が仮決めの型
 
+//ジェネリクス関数
+fun <T> box(value:T):Container<T> = Container(value)
+
+val <T> T.string:String
+    get() = toString()
+
 fun main(args:Array<String>){
+    //導入
     val intContainer:Container<Int> = Container<Int>(123)
     val i:Int = intContainer.value
     println(i*2)
 
     val strContainer = Container("hello") //型引数を省略した
     println(strContainer.value)
+
+    //ジェネリクス関数
+    val container:Container<Int> = box(5)
+    println(container.value)
+    println(container.string)
 }
